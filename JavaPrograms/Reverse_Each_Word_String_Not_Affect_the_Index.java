220720e057cf75d88c30747892fcbde69c128b19
Arun-Suresh-Common <arunsureshcommon@gmail.com>
2024-09-20 22:44:40 +0530

    Update Reverse_Each_Word_String_Not_Affect_the_Index.java

--- JavaPrograms/Reverse_Each_Word_String_Not_Affect_the_Index.java
+++ JavaPrograms/Reverse_Each_Word_String_Not_Affect_the_Index.java
@@ -1,9 +1,16 @@
 package programs;
-
+import java.util.Arrays;
+import java.util.stream.Collectors;
 public class Reverse_Each_Word_String_Not_Affect_the_Index {
 
 	public static void main(String... args) {
+                String input = "alex brian charles";
+		String reversed = Arrays.stream(input.split(" ")).map(word -> new StringBuilder(word).reverse())
+				.collect(Collectors.joining(" "));
 
+		System.out.println(reversed);
+
+		// Second program
 		String s = "arun kumar";
 
 		String str[] = s.split(" ");
@@ -12,7 +19,7 @@ public class Reverse_Each_Word_String_Not_Affect_the_Index {
 			String revword = "";
 			String word = str[i];
 
-			for (int j = word.length()-1; j >= 0; j--) {
+			for (int j = word.length() - 1; j >= 0; j--) {
 
 				revword = revword + word.charAt(j);
 			}
