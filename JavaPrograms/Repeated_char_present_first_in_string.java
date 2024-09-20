4d83a7fd5b51fdc37291c2a36fb72452b63bb78b
Arun-Suresh-Common <arunsureshcommon@gmail.com>
2024-09-20 22:31:25 +0530

    Update Repeated_char_present_first_in_string.java

--- JavaPrograms/Repeated_char_present_first_in_string.java
+++ JavaPrograms/Repeated_char_present_first_in_string.java
@@ -4,23 +4,10 @@ import java.util.HashMap;
 
 public class Repeated_char_present_first_in_string {
 	public static void main(String[] args) {
-	
-	String str = "weeksforgeeks";
-	HashMap<Character, Integer> freq
-            = new HashMap<Character, Integer>();
-        for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-            freq.put(c, freq.getOrDefault(c, 0) + 1);
-        }
- 
-        // Traverse the string
-        for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-            if (freq.get(c) > 1) {
-                System.out.println(c);
-                break;
-            }
-        }
+	   String str = "yourStringHere";
+        for (char c : str.toCharArray()) if (str.indexOf(c) != str.lastIndexOf(c))
+	{ System.out.println(c); break; }
+	  
 	}
 
 }
