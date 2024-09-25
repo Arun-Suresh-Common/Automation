b2c5c1a0ef95ee8546557235e5aa6bb9c70a425d
Arun-Suresh-Common <arunsureshcommon@gmail.com>
2024-09-25 19:44:54 +0530

    Update String_Split_Char_Numeric_SpecialChar.java

--- JavaPrograms/String_Split_Char_Numeric_SpecialChar.java
+++ JavaPrograms/String_Split_Char_Numeric_SpecialChar.java
@@ -3,6 +3,30 @@ package programs;
 public class String_Split_Char_Numeric_SpecialChar {
 	public static void main(String[] args) {
 
+	        //Without using inbuilt function
+//Without using inbuilt function
+	
+        String str = "Java11000!@#456ABC";
+        StringBuilder alphabets = new StringBuilder();
+        StringBuilder numbers = new StringBuilder();
+        StringBuilder specialChars = new StringBuilder();
+
+        for (int k = 0; k < str.length(); k++) {
+            char ch = str.charAt(k);
+            if (ch >= '0' && ch <= '9') {
+                numbers.append(ch);
+            } else if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+                alphabets.append(ch);
+            } else {
+                specialChars.append(ch);
+            }
+        }
+
+        System.out.println("Alphabets: " + alphabets.toString());
+        System.out.println("Numbers: " + numbers.toString());
+        System.out.println("Special Characters: " + specialChars.toString());
+
+		
 		String s = "ar5un^&^!@5ku#%*)";
 		StringBuilder str1 = new StringBuilder(), str2 = new StringBuilder(), str3 = new StringBuilder();
 		for (int i = 0; i < s.length(); i++) {
