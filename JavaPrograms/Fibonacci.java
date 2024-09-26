08b15a28a77636b3ce9060189234705d0e9f2b2e
Arun-Suresh-Common <arunsureshcommon@gmail.com>
2024-09-26 16:37:52 +0530

    Update Fibonacci.java

--- JavaPrograms/Fibonacci.java
+++ JavaPrograms/Fibonacci.java
@@ -1,10 +1,18 @@
-int n = 10;
-    int num1 = 0, num2 = 1;
+package a1;
 
-        for (int i = 0; i < n; i++) {
-            System.out.print(num1 + " ");
-            // Swap
-            int num3 = num1 + num2;
-            num1 = num2;
-            num2 = num3;
+public class Fibonacci {
+
+	public static void main(String[] args) throws Exception {
+		int n = 10;
+		int num1 = 0, num2 = 1;
+
+		for (int i = 0; i < n; i++) {
+			System.out.print(num1 + " ");
+			// Swap
+			int num3 = num1 + num2;
+			num1 = num2;
+			num2 = num3;
+		}
 	}
+
+}
