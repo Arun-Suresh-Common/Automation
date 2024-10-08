769fc79e706a76bf11fc985c47199c4319af1f05
Arun-Suresh-Common <arunsureshcommon@gmail.com>
2024-10-08 11:00:43 +0530

    Update Reverse_String_preserving_space_positions.java

--- JavaPrograms/Reverse_String_preserving_space_positions.java
+++ JavaPrograms/Reverse_String_preserving_space_positions.java
@@ -2,42 +2,23 @@ package programs;
 
 public class Reverse_String_preserving_space_positions {
 	public static void main(String[] args) {
-		 String str = "am at geeks for geeks";
-		 int n = str.length();
-		  
-		    // Initialize two pointers as two corners
-		    int start = 0;
-		    int end = n - 1;
-		     
-		    char[] Str = str.toCharArray();
-		     
-		    // Move both pointers toward each other
-		    while (start < end)
-		    {
-		         
-		        // If character at start or end 
-		        // is space, ignore it
-		        if (Str[start] == ' ') 
-		        {
-		            start++;
-		            continue;
-		        }
-		        else if (Str[end] == ' ') 
-		        {
-		            end--;
-		            continue;
-		        }
-		  
-		        // If both are not spaces, do swap
-		        else
-		        {
-		            char temp = Str[start];
-		            Str[start] = Str[end];
-		            Str[end] = temp;
-		            start++;
-		            end--;
-		        }
-		    }
-		    System.out.println(String.valueOf(Str));
+	String a = "arun em f tew";
+
+	char ch[] = a.toCharArray();
+	int i=0,j=ch.length-1;
+	while(i<j){
+	    if(ch[i]==' '){i++;continue;}
+	
+	   else if(ch[j]==' '){j--;continue;}
+	
+	else{
+	    char temp = ch[i];
+	    ch[i++] = ch[j];
+	    ch[j--] = temp;
+	   // i++;
+	   // j--;
+	}
+	}
+	System.out.println(new String(ch));
 	}
 }
